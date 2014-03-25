a99e66b8d1b5a0207ac1a86bee3fafd9b15b190c
Drahflow <drahflow@gmx.de>
2014-03-25 09:50:15 +0100

    How about Schleswig-Holstein?

--- Parse.pm
+++ Parse.pm
@@ -2,13 +2,176 @@ package Parse;
 
 use strict;
 use warnings;
-use encoding 'utf-8';
+use utf8;
 use Storable;
 
 use Data::Dumper;
 use XML::Twig;
 
 my %all;
+
+sub parseHTMLSH {
+  my ($source, $silent) = @_;
+
+  if(-r "$source.store") {
+    my $law = retrieve("$source.store");
+    $all{$law->{'name'} . '(' . $law->{'veryshorthand'} . ')'} = $law;
+
+    print "Loaded " . $law->{'name'} . ' (' . $law->{'veryshorthand'} . ")\n" unless $silent;
+
+    return;
+  }
+
+  print "Parsing $source...\n";
+
+  open HTML, '<:utf8', $source or die "cannot open $source: $!";
+  my $xml = XML::Twig->new();
+  $xml->parse(join('', <HTML>));
+  close HTML;
+
+
+  my $content = $xml->root();
+  my $veryshorthand = saneSpacing([$content->get_xpath('//table[@class="TableRahmenkpl"]//td[@class="TD70"]')]->[0]->text());
+  my $name = [$content->get_xpath('//div[@class="docLayoutTitel"]')]->[0]->text();
+  $name =~ s/^\s+Vom.*$//gm;
+  $name = saneSpacing($name);
+
+  my $shorthand = $veryshorthand;
+  if($name =~ /\(([^)]+)\)/) {
+    $shorthand = $1;
+    $shorthand =~ s/ - .*//g;
+    $shorthand = saneSpacing($shorthand);
+  }
+
+  print "$shorthand: $name\n";
+
+  my @paragraphs;
+  
+  my @blocks = $content->get_xpath('//div[@class="docLayoutText"]//div[@class="docLayoutMarginTop"]');
+  foreach my $block (@blocks) {
+    my ($h3) = $block->get_xpath('h3');
+    last if($h3 and saneSpacing($h3->text()) =~ /^Anlage( A)?:$/);
+
+    my ($header) = $block->get_xpath('h3[@class="paranr"]');
+    my ($headerTitle) = $block->get_xpath('h3[@class="paratitel"]');
+    unless($header) {
+      ($header) = $block->get_xpath('h4[@class="paranr"]');
+      ($headerTitle) = $block->get_xpath('h4[@class="paratitel"]');
+    }
+    unless($header) {
+      ($header) = $block->get_xpath('h5[@class="paranr"]');
+      ($headerTitle) = $block->get_xpath('h5[@class="paratitel"]');
+    }
+    unless($header) {
+      ($header) = $block->get_xpath('h6[@class="paranr"]');
+      ($headerTitle) = $block->get_xpath('h6[@class="paratitel"]');
+    }
+    unless($header) {
+      ($header) = $block->get_xpath('p[@class="paranr"]');
+      ($headerTitle) = $block->get_xpath('p[@class="paratitel"]');
+    }
+    next unless $header;
+
+    my $paragraphNumber = saneSpacing($header->text());
+    my $paragraphTitle;
+    if($headerTitle) {
+      $paragraphTitle = saneSpacing($headerTitle->text());
+    }
+
+    if($paragraphNumber =~ /^§ ?(\d+)\s*(\S*)$/s) {
+      my $trailing = $2;
+      $trailing =~ s/\*\)//g;
+      $trailing =~ s/\^\[?\d+\]?//g;
+      my $paragraph = {
+        'number' => "$1$trailing",
+        'absatz' => [],
+      };
+      $paragraph->{'title'} = $paragraphTitle if($paragraphTitle);
+      push @paragraphs, $paragraph;
+
+      parseAbsatzSH($block, $paragraph);
+
+      print "Parsed § " . $paragraph->{'number'} . ' ' . ($paragraph->{'title'}? $paragraph->{'title'}: "") . "\n" unless $silent;
+    } elsif($paragraphNumber =~ /^(?:Art\.?|Artikel) (\S*)$/s) {
+      my $paragraph = {
+        'number' => "Artikel $1",
+        'title' => $2,
+        'absatz' => [],
+      };
+      push @paragraphs, $paragraph;
+
+      parseAbsatz($block, $paragraph);
+
+      print "Parsed " . $paragraph->{'number'} . ' ' . ($paragraph->{'title'}? $paragraph->{'title'}: "") . "\n" unless $silent;
+    } elsif($paragraphNumber =~ /^([IVXLCDM]+)\.\s*(.*)$/s) {
+      my $paragraph = {
+        'number' => "Artikel $1",
+        'title' => $2,
+        'absatz' => [],
+      };
+      push @paragraphs, $paragraph;
+
+      parseAbsatz($block, $paragraph);
+
+      print "Parsed " . $paragraph->{'number'} . ' ' . ($paragraph->{'title'}? $paragraph->{'title'}: "") . "\n" unless $silent;
+    } elsif($paragraphNumber =~ /^(?:Einziger )?Artikel$/s) {
+      my $paragraph = {
+        'number' => "Artikel 1",
+        'absatz' => [],
+      };
+      push @paragraphs, $paragraph;
+
+      parseAbsatz($block, $paragraph);
+
+      print "Parsed " . $paragraph->{'number'} . ' ' . ($paragraph->{'title'}? $paragraph->{'title'}: "") . "\n" unless $silent;
+    } elsif($paragraphNumber =~ /^§§? \d+ bis \d+(?:\*\))?$/s) {
+      # skip
+    } elsif($paragraphNumber =~ /^§§? \d+ - \d+(?:\*\))?$/s) {
+      # skip
+    } elsif($paragraphNumber =~ /^§§? \d+ u\. \d+(?:\*\))?$/s) {
+      # skip
+    } else {
+      print $paragraphNumber;
+      die;
+    }
+  }
+
+  print "Parsed $name ($veryshorthand)\n" unless $silent;
+
+  my $law = {
+    'name' => $name,
+    'shorthand' => $shorthand,
+    'veryshorthand' => $veryshorthand,
+    'paragraph' => \@paragraphs,
+    'source' => $source,
+  };
+
+  store $law, "$source.store";
+  $all{$law->{'name'} . '(' . $law->{'veryshorthand'} . ')'} = $law;
+}
+
+sub parseAbsatzSH {
+  my ($block, $paragraph) = @_;
+
+  my @absatz = $block->get_xpath('p');
+  foreach my $absatz (@absatz) {
+    my $text = $absatz->text();
+    $text =~ s/\s+ / /g;
+
+    if($text =~ /^\((\d+)\S*\) (.*)$/s) {
+      $absatz = {
+        'number' => $1,
+        'text' => $2,
+      };
+    } else {
+      $absatz = {
+        'text' => $text,
+      };
+    }
+
+    push @{$paragraph->{'absatz'}}, $absatz;
+  }
+}
 
 sub parseHTML {
   my ($source, $silent) = @_;
@@ -171,13 +334,15 @@ sub parseHTML {
         print "Parsed " . $paragraph->{'number'} . ' ' . $paragraph->{'title'} . "\n" unless $silent;
       } elsif($h3->text() =~ /\(Änderungs- und Aufhebungsvorschriften\)/) {
         # skip
-      } elsif($h3->text() =~ /\(weggefallen\)/) {
+      } elsif($h3->text() =~ /weggefallen/) {
         # skip
       } elsif($h3->text() =~ /\(zukünftig in Kraft\)/) {
         # skip
       } elsif($h3->text() =~ /----/) {
         # skip
       } elsif($h3->text() =~ /\(gegenstandslos\)/) {
+        # skip
+      } elsif($h3->text() =~ /\(Änderung von Rechtsvorschriften, Überleitung von Verweisungen, Aufhebung von Vorschriften\)/) {
         # skip
       } elsif($h3->text() =~ /Schlu(ß|ss)formel/) {
         # skip
@@ -193,6 +358,8 @@ sub parseHTML {
         # skip
       } elsif($h3->text() =~ /^Gesamtplan /) {
         # skip
+      } elsif($h3->text() =~ /Nachtrag zum Gesamtplan des Bundeshaushaltsplans/) {
+        # skip
       } elsif($block->descendants('img')) {
         # skip
       } elsif($h3->text() =~ /^Vorschriften für/) {
@@ -200,10 +367,13 @@ sub parseHTML {
         # skip
       } else {
         $block->print();
+        print "\n" . $h3->text() . "\n";
+        print $name . "\n";
         die;
       }
     } else {
       $block->print();
+      print $name . "\n";
       die;
     }
   }
@@ -251,6 +421,14 @@ sub parseAbsatz {
 
     push @{$paragraph->{'absatz'}}, $absatz;
   }
+}
+
+sub saneSpacing {
+  my ($s) = @_;
+  $s =~ s/\s+/ /g;
+  $s =~ s/^\s+//g;
+  $s =~ s/\s+$//g;
+  return $s;
 }
 
 1;
